03f16389116f8b0788e0925f1850f3c31f184448
marco <marcodecrema@libero.it>
2020-09-15 15:44:08 +0200

    lista dei comandi da svolgere

--- js/script.js
+++ js/script.js
@@ -1,1 +1,5 @@
-alert("ciao");
+//  Creare un oggetto che descriva uno studente: nome, cognome, età
+// Stampare le proprietà dell'oggetto attraverso un ciclo 'for in'
+// Creare un array di oggetti di studenti
+// Ciclare su tutti gli studenti e stampare nome e cognome
+// L'utente aggiunge un nuovo oggetto composto da: nome cognome e età
