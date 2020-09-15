c6ddb8f3062199096bae34e8e20ec76213540eac
marco <marcodecrema@libero.it>
2020-09-15 16:03:12 +0200

    lista studenti con rispettive proprietà e richiamo in console

--- js/script.js
+++ js/script.js
@@ -1,13 +1,36 @@
 //  Creare un oggetto che descriva uno studente: nome, cognome, età
-var studente1 = {
+var studente = {
   'nome': 'piero',
   'cognome': 'rossi',
   'eta': 15
 }
 // Stampare le proprietà dell'oggetto attraverso un ciclo 'for in'
-for (var key in studente1) {
-  console.log(studente1[key]);
+for (var key in studente) {
+  console.log(studente[key]);
 }
 // Creare un array di oggetti di studenti
+var listaStudenti = [
+  {
+    'nome': 'piero',
+    'cognome': 'rossi',
+    'eta': 15
+  },
+  {
+  'nome': 'luigi',
+  'cognome': 'bianchi',
+  'eta': 18
+  }
+];
+
+var listaStudenti.push(studente);
 // Ciclare su tutti gli studenti e stampare nome e cognome
+for (i = 0; i < listaStudenti.length; i++) {
+
+  for (var key2 in listaStudenti[i]) {
+    var studenti = listaStudenti[i];
+    console.log(studenti[key2]);
+  }
+
+}
 // L'utente aggiunge un nuovo oggetto composto da: nome cognome e età
+var nome
