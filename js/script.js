d8801377d51679c9f284bb9d6c174958bb552b42
marco <marcodecrema@libero.it>
2020-09-15 15:50:05 +0200

    oggetto studente e richiamo delle proprietà

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
 //  Creare un oggetto che descriva uno studente: nome, cognome, età
+var studente1 = {
+  'nome': 'piero',
+  'cognome': 'rossi',
+  'eta': 15
+}
 // Stampare le proprietà dell'oggetto attraverso un ciclo 'for in'
+for (var key in studente1) {
+  console.log(studente1[key]);
+}
 // Creare un array di oggetti di studenti
 // Ciclare su tutti gli studenti e stampare nome e cognome
 // L'utente aggiunge un nuovo oggetto composto da: nome cognome e età
